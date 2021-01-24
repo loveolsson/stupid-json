244fe91656f62bc59ba9a63ffb70dc695a989e7c
Love Olsson <love@loveolsson.se>
2021-01-25 00:50:47 +0100

    Fixing debugging

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ cd $reldir
 
 mkdir -p build-tests
 cd build-tests
-cmake -DCMAKE_BUILD_TYPE=Release ../tests/
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../tests/
+#cmake -DCMAKE_BUILD_TYPE=Debug ../tests/
 make -j
 ctest -V
